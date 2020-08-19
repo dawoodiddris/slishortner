91a1c6263bbcda8b0327fd903f88cae30a84e091
dawoodiddris <dawoodiddris@gmail.com>
2020-08-19 07:57:46 -0700

    added session

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
+app.secret_key = 'fghjklkjhgfdfghjktrertyuytf'
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', emails=session.keys())
 
 @app.route('/urlshortner')
 def urlshortner():
@@ -13,5 +15,5 @@ def urlshortner():
 
 @app.route('/data', methods=['GET','POST'])
 def data():
-
-    return render_template('data.html', name=request.form['email'])
+    session[request.form['email']] = True
+    return render_template('data.html', email=request.form['email'])
